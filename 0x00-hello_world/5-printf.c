7c61c51dbc61ac524394b092c385e838ad99a221
Richie0003 <asansirich45@gmail.com>
2022-07-02 11:14:00 +0000

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: Always 0 (success)
- */
+/*main -entry point*/
+/*always return 0 (success)*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
